262c172fa6491f932aacdcf1b5a0cd120f691da4
Tomokatsu Iguchi <tomo14916253@gmail.com>
2018-05-25 00:12:10 +0900

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Tomokatsu Iguchi on 2018/05/25.
+//  Copyright © 2018年 Tomokatsu Iguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
